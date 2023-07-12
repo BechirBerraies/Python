70f65405cf3b1036fb3a101b701f30e0bf941033
BechirBerraies <136343630+BechirBerraies@users.noreply.github.com>
2023-07-12 17:03:42 +0100

    Basketball

---
+++ 02-python-oop/Week1 -Day2/Basketball.py
@@ -0,0 +1,49 @@
+class Player:
+
+    NewTeam = []
+    def __init__(self,info):
+        self.name = info['name']
+        self.age = info['age']
+        self.position = info['position']
+        self.team = ['team']
+        self.NewTeam.append()
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+# player_jason = ???
+
+print(kevin)
+
+
+    
+# Create your Player instances here!
+# player_jason = ???
+
+
+    
+# Create your Player instances here!
+# player_jason = ???
+
+
+
+def displayAll(self):
+    print()
+
